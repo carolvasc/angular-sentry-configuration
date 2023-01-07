09b776c4e3b591a65ef61657a9526b973e5d5893
Carol Vasconcelos <cvsouza04@gmail.com>
2023-01-07 12:58:13 -0300

    config: installing and first configuration of sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { Router } from "@angular/router";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import * as Sentry from "@sentry/angular";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,24 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => { },
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from "@sentry/tracing";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
+Sentry.init({
+  dsn: "https://e63f7a001e954c9699d8e4ef44b70274@o4504406526590977.ingest.sentry.io/4504464046948352",
+  integrations: [
+    new BrowserTracing({
+      tracePropagationTargets: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+});
+
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
   .catch(err => console.error(err));
