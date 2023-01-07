42d563c8ae3c886a6fb0b25d678eec7a85704b72
Carol Vasconcelos <cvsouza04@gmail.com>
2023-01-07 13:21:14 -0300

    feat: proposital error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Sentry from "@sentry/angular";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-sentry-configuration';
+  undefinedObj: any;
+
+  public ngOnInit(): void {
+    try {
+      this.title = this.undefinedObj.title;
+    } catch(err) {
+      Sentry.captureException(err);
+    }
+  }
 }
